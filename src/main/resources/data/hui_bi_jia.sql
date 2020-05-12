3c11bf6c3874a8e5c710d1b6358fd4c705a51ccc
heyhuo@163.com <heyhuo@163.com>
2020-05-12 11:08:29 +0800

    数据库表结构

--- src/main/resources/data/hui_bi_jia.sql
+++ src/main/resources/data/hui_bi_jia.sql
@@ -1,19 +1,30 @@
 create database nice_price;
 use nice_price;
 -- ----商品----
-create table goods_info
+-- ----------------------------
+-- Table structure for goods_info
+-- ----------------------------
+DROP TABLE IF EXISTS `goods_info`;
+CREATE TABLE `goods_info`
 (
-    goods_id    varchar(30) primary key not null COMMENT '电商商品id',
-    goods_name  varchar(255) COMMENT '名称',
-    price       varchar(9) COMMENT '价格',
-    variety     varchar(20) COMMENT '商品种类',
-    tag         varchar(255) COMMENT '商品标签',
-    detail      text COMMENT '详细描述',
-    pic_address text COMMENT '图片地址',
-    link        text COMMENT '链接',
-    plat_form   varchar(20) COMMENT '平台',
-    ave_comment varchar(3) COMMENT '评分均值'
-) COMMENT ='商品';
+    `goods_id`    varchar(30) NOT NULL COMMENT '电商商品id',
+    `goods_name`  varchar(255) DEFAULT NULL COMMENT '名称',
+    `price`       varchar(9)   DEFAULT NULL COMMENT '价格',
+    `variety`     varchar(20)  DEFAULT NULL COMMENT '商品种类',
+    `tag`         mediumtext CHARACTER SET utf8 COLLATE utf8_general_ci COMMENT '商品标签',
+    `detail`      text COMMENT '详细描述',
+    `pic_address` text COMMENT '图片地址',
+    `link`        text COMMENT '链接',
+    `plat_form`   varchar(20)  DEFAULT NULL COMMENT '平台',
+    `ave_comment` varchar(3)   DEFAULT NULL COMMENT '评分均值',
+    `craw_date`   datetime     DEFAULT NULL COMMENT '爬取时间',
+    `month_sale`  int          DEFAULT NULL COMMENT '月销量',
+    `comment_num` int          DEFAULT NULL COMMENT '评价数量',
+    PRIMARY KEY (`goods_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='商品';
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 # 插入测试
 # insert into
@@ -37,12 +48,18 @@ Create table comments
 ) COMMENT ='评价';
 
 -- ----价格储存----
-Create table price_save
+-- ----------------------------
+-- Table structure for price_save
+-- ----------------------------
+DROP TABLE IF EXISTS `price_save`;
+CREATE TABLE `price_save`
 (
-    goods_id   varchar(30) COMMENT '商品id',
-    plat_form  varchar(20) COMMENT '平台',
-    price_time datetime COMMENT '价格时间',
-    price      varchar(9) COMMENT '价格',
-    primary key (goods_id, price_time),
-    foreign key (goods_id) references goods_info (goods_id)
-) COMMENT ='价格储存';
+    `goods_id`   varchar(30) NOT NULL COMMENT '商品id',
+    `price_time` datetime    NOT NULL COMMENT '价格时间',
+    `price`      int                                                    DEFAULT NULL COMMENT '价格',
+    `plat_form`  varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '平台',
+    PRIMARY KEY (`goods_id`, `price_time`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT ='价格储存';
+
+SET FOREIGN_KEY_CHECKS = 1;
